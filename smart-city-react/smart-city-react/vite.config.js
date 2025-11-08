01eed388ca4ddcf6b024550b425b315d858d270f
sasank6004 <sasanksammeta6@gmail.com>
2025-11-08 12:28:02 +0530

    Update vite config and package-lock.json

--- smart-city-react/smart-city-react/vite.config.js
+++ smart-city-react/smart-city-react/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
